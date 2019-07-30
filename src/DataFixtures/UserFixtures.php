45df8e430d6daffa4f0c508187490f24ff010953
Parmentier <fparmentier@free.fr>
2019-07-30 17:57:30 +0200

    201907301757

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -24,14 +24,12 @@ class UserFixtures extends Fixture
     * Created   : 2019/07/29
     *
     * @param string $firstName
-    * @param string $lastName
     * @return string
     */
-    private function createPseudo(string $firstName, string $lastName): string {
+    private function createPseudo(string $firstName): string {
         $number=(string)mt_rand(120,999);
         $firstN=(strlen($firstName)>5) ? substr($firstName,0,strlen($firstName)-3) : $firstName;
-        $lastN=(strlen($lastName)>5) ? substr($lastName,0,strlen($lastName)-3) : $lastName;
-        $pseudo=$firstN.$number;
+        $pseudo=mb_strtolower($firstN).$number;
         dump($pseudo);
         return $pseudo;
     }
@@ -55,17 +53,15 @@ class UserFixtures extends Fixture
             $pictureId=$faker->numberbetween(1,99) . '.jpg';
             $picture .= ($gender=='male' ? 'men/' : 'women/') .$pictureId;
 
-            $createdAt=new \DateTime();
-
-            $user   -> setPseudo($this->createPseudo($faker->firstName($gender), $faker->lastName($gender)))
+            $user   -> setPseudo($this->createPseudo($faker->firstName($gender)))
                     -> setEmail($faker->email)
                     -> setPassword($this->passwordEncoder->encodePassword($user,'password'))
                     -> setAvatar($picture)
                     -> setBirthdayDate($birthDate)
                     -> setAge(30)
                     -> setDescription($faker->sentence())
-                    -> setCreatedAt($createdAt)
-                    -> setLastModified(new \DateTime());
+                    -> setCreatedAt(new \DateTime('now'))
+                    -> setLastModified(new \DateTime('now'));
 
             $manager->persist($user);
         }
